612630ec3eae0b39e528677cdd862126af596866
LAGHZALI Taha <mrtahalz@gmail.com>
2015-09-30 12:14:42 +0200

    t

---
+++ public/js/text-anim.js
@@ -0,0 +1,16 @@
+var wss_i = 0;
+var wss_array = ["lire","apprendre","comprendre","etre intelligent"];
+var wss_elem;
+function wssNext(){
+	wss_i++;
+	wss_elem.style.opacity = 0;
+	if(wss_i > (wss_array.length - 1)){
+		wss_i = 0;
+	}
+	setTimeout('wssSlide()',1000);
+}
+function wssSlide(){
+	wss_elem.innerHTML = wss_array[wss_i];
+	wss_elem.style.opacity = 1;
+	setTimeout('wssNext()',1000);
+}
