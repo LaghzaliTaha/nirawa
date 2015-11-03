d8ca18ddc015dd8e0ee97f01e0d586b39c2b49d9
LAGHZALI Taha <mrtahalz@gmail.com>
2015-11-03 17:18:09 +0100

    hsgdf

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
 });
 /*********/
 app.use(express.static(__dirname + '/public'));
-//app.use(express.static(__dirname + '/views/'));
+app.use(express.static(__dirname + '/views/'));
 //app.use(express.static(__dirname + '/views/nirawa'));
 app.use('/admin', adminRoutes);
 app.use('/nirawa', userRoutes);
