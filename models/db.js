e6d16e00b7c67a7a07b31d3184613ecf02b52e51
LAGHZALI taha <mrtahalz@gmail.com>
2016-03-09 16:16:44 +0100

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://root:root@ds031213.mongolab.com:31213/nirawa');
+mongoose.connect('mongodb://root:root@ds031213.mongolab.com:31213/stone');
 //mongoose.connect('mongodb://root:root@ds029117.mongolab.com:29117/stone');
 //mongoose.connect('mongodb://motogba:123456@ds041821.mongolab.com:41821/user');
 
